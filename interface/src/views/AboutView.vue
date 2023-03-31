6afd06a27b6530813e950d1be9235db192271ac3
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2023-03-31 10:35:35 -0300

    setup interface project

---
+++ interface/src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
---
+++ interface/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+<template>
+  <div class="about">
+    <h1>This is an home page</h1>
+    <RouterLink to="/about">About</RouterLink>
+  </div>
+</template>
